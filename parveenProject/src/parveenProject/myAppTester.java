31e6af8080cd1e1721937c63961635ccdd1989eb
parveen <parveen@gmail.com>
2020-05-20 20:13:29 -0700

    Program running

--- parveenProject/src/parveenProject/myAppTester.java
+++ parveenProject/src/parveenProject/myAppTester.java
@@ -13,8 +13,14 @@ public class myAppTester {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Hello Software Engineering World");
+		System.out.println(AddMethod(5,8));
 	}
+public static int AddMethod(int num1, int num2){
+int k  = num1 + num2;
+return k;
+}
+
 
 }
